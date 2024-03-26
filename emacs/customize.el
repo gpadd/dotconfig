b6bd67ac6d36f68469504c50502f27007d3ca8cc
Andreas Wilfer <andreas@wilfer.se>
2024-03-26 14:40:24 +0100

    Add company to package list

--- emacs/customize.el
+++ emacs/customize.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit evil evil-collection evil-tutor paredit circe)))
+   '(company magit evil evil-collection evil-tutor paredit circe)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
