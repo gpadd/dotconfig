fe312dea413edb0200549ef373563c5321aa93f6
Andreas Wilfer <andreas@wilfer.se>
2024-10-23 13:58:30 +0200

    Some tweaks for three modes

--- emacs/init.el
+++ emacs/init.el
@@ -18,9 +18,12 @@
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(tooltip-mode -1)
+(column-number-mode 1)
+(show-paren-mode 1)
+;;(tooltip-mode -1)
 ;;(menu-bar-mode -1)
 
+;;(setq display-line-numbers-type 't)
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
 
